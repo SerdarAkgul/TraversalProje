6cd0cfbbbf65ef0530c528429e480b81d0092186
SerdarAkgul <114110355+SerdarAkgul@users.noreply.github.com>
2023-09-11 00:51:59 +0300

    TraversalProje

---
+++ TraversalCoreProje/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrate;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Statistics : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            using var c = new Context();
+            ViewBag.v1 = c.Destinations.Count();
+            ViewBag.v2 = c.Guides.Count();
+            ViewBag.v2 = "285";
+            return View();
+        }
+    }
+}
